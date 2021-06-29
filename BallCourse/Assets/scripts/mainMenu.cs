171641f0695efc93c8da867080d0cebca0814967
Alien <kendgibsonjr@gmail.com>
2021-06-29 12:09:58 -0500

    added start menu

---
+++ BallCourse/Assets/scripts/mainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+   public void PlayGame (){
+       //Loads the next level by getting the index of the current scnece and ading one
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void QuitGame (){
+       Application.Quit();
+   }
+}

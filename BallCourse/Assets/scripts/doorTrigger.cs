6afe45d1f6a992ee1559111c67fbe07b7d596775
Alien <kendgibsonjr@gmail.com>
2021-07-05 20:26:02 -0500

    added event for door area collison

---
+++ BallCourse/Assets/scripts/doorTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorTrigger : MonoBehaviour
+{
+
+    [SerializeField] private Animator door = null;
+
+    [SerializeField] private bool openTrigger = false;
+    
+
+   private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object Entered the trigger");
+        if (other.CompareTag("Player")){
+
+            if (openTrigger)
+            {
+                Debug.Log("we here");
+                door.Play("door open", 0, 0.0f);
+                
+                
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        openTrigger = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Object Exited the trigger");
+           if (other.CompareTag("Player")){
+
+            if (!openTrigger)
+            {
+                door.Play("Door close", 0, 0.0f);
+                openTrigger = true;
+                
+            }
+        }
+    }
+}

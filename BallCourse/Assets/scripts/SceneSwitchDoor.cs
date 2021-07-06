4fdd373622b5d4f735df980d3216cde8b28eb36d
Alien <kendgibsonjr@gmail.com>
2021-07-05 22:36:28 -0500

    set up door for scene transition

---
+++ BallCourse/Assets/scripts/SceneSwitchDoor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitchDoor : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("we good to switch scenes");
+        
+    }
+}

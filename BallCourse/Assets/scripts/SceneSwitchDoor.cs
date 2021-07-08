644ff817306303a6bc29a9bb7728fec3b682c138
Alien <kendgibsonjr@gmail.com>
2021-07-08 13:35:27 -0500

    added scene transition through door

--- BallCourse/Assets/scripts/SceneSwitchDoor.cs
+++ BallCourse/Assets/scripts/SceneSwitchDoor.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitchDoor : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("we good to switch scenes");
+        SceneManager.LoadScene(2);
         
     }
 }

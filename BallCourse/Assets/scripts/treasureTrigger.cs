4e002b051e3b20ff08b4744bd86d7b6d6d804832
Alien <kendgibsonjr@gmail.com>
2021-07-19 17:45:30 -0500

    added item pickup

---
+++ BallCourse/Assets/scripts/treasureTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treasureTrigger : MonoBehaviour
+{
+
+    public bool fadeOut;
+   
+    
+
+    public void OnTriggerEnter(Collider Player)
+    {
+        
+        fadeOut = true;
+
+        Debug.Log("you found an object");
+        
+        triggerListen();
+
+    }
+
+    public void triggerListen(){
+         
+        if(fadeOut)
+        {
+            Debug.Log("fadeOut");
+            fadeOut = false;
+        }
+
+    }
+
+
+
+  
+}
